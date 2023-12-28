3420853d4a4444afd2ca8fb52cc04095b25fbb47
Pedro Souza <mypedrosouza@gmail.com>
2023-12-28 19:45:38 -0300

    Data keys

---
+++ src/main/java/com/github/eupedroosouza/messaging/data/DataKeys.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright � 2023 Pedro Souza
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the �Software�), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED �AS IS�, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.github.eupedroosouza.messaging.data;
+
+public class DataKeys {
+    public static final String CORRELATION_ID_KEY = "correlationId";
+    public static final String MESSAGE_KEY = "message";
+    public static final String REMOTE_TIMEOUT_KEY = "remoteTimeout";
+    public static final String RESPONSE_KEY = "response";
+    public static final String ERROR_CLASS_NAME_KEY = "errorClassName";
+    public static final String ERROR_MESSAGE_KEY = "errorMessage";
+    public static final String CLASS_NAME_KEY = "className";
+}
